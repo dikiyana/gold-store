e284597b8587c7aa01a31f6191066ec81c865cd8
Dikiyana Pratama <dikiyana.pratama@gmail.com>
2026-01-17 23:51:38 +0700

    update gramasi

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-const hargaBeli = 1050000;
-const hargaJual = 1020000;
+const hargaEmas = {
+  0.5: 1597000,
+  1: 3054000,
+  2: 6040000,
+  3: 9032000,
+  5: 14615000,
+  10: 29080000,
+  25: 72000000,
+  50: 143200000,
+  100: 284400000
+};
 
 function hitung() {
   const jenis = document.getElementById("jenis").value;
-  const berat = document.getElementById("berat").value || 0;
+  const gramasi = document.getElementById("gramasi").value;
 
-  let total = jenis === "beli"
-    ? berat * hargaBeli
-    : berat * hargaJual;
+  let harga = hargaEmas[gramasi];
+
+  if (jenis === "jual") {
+    // contoh buyback lebih rendah
+    harga = harga - 5000 * gramasi;
+  }
 
   document.getElementById("total").innerText =
-    total.toLocaleString("id-ID");
+    harga.toLocaleString("id-ID");
 }
